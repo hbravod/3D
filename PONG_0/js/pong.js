e26e73ef5f1f6dd16bf605f488e13d16e40b64fa
hbravod <32259113+hbravod@users.noreply.github.com>
2018-04-17 09:40:36 +0200

    añadiendo rebote y marcador

--- PONG_0/js/pong.js
+++ PONG_0/js/pong.js
@@ -14,12 +14,15 @@ const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
 const FAR = 10000;
 
+var score1 = 0, score2 = 0, maxScore = 5;
+
+// set opponent reaction rate (0-easiest, 1-hardest)
+var difficulty = 0.2;
+
 // Scene object variables
 var renderer, scene, camera, pointLight;
 
-var sphere;
-
-var cubePlayer, cubeCPU, plane;
+var sphere, paddlePlayer, paddleCPU, plane;
 
 const FIELD_WIDTH = 400,
       FIELD_HEIGHT = 200;
@@ -49,10 +52,11 @@ function setup()
     createScene();
       addSphereMesh();
       addPlaneMesh();
-      addCubeMeshPlayer();
-      addCubeMeshCPU();
+      addPaddleMeshPlayer();
+      addPaddleMeshCPU();
       addLight();
-      //draw();
+      score();
+      //rebound();
       requestAnimationFrame(draw); // redibujar la esfera todo el rato.
 }
 
@@ -113,28 +117,30 @@ function addPlaneMesh(){
   plane.position.z = -300;
 }
 
-function addCubeMeshPlayer(){
+function addPaddleMeshPlayer(){
   var geometry = new THREE.BoxGeometry(PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_DEPTH);
   var material = new THREE.MeshLambertMaterial(
       {
         color: '#2E9AFE'
       });
-  cubePlayer = new THREE.Mesh( geometry, material );
-  scene.add( cubePlayer );
-  cubePlayer.position.z = -300;
-  cubePlayer.position.x = -190;
+  paddlePlayer = new THREE.Mesh( geometry, material );
+  scene.add( paddlePlayer );
+  paddlePlayer.position.z = -300;
+  paddlePlayer.position.x = -190;
 }
 
-function addCubeMeshCPU(){
+function addPaddleMeshCPU(){
   var geometry = new THREE.BoxGeometry(PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_DEPTH);
   var material = new THREE.MeshLambertMaterial(
       {
         color: '#FF0000'
       });
-  cubeCPU = new THREE.Mesh( geometry, material );
-  scene.add( cubeCPU );
-  cubeCPU.position.z = -300;
-  cubeCPU.position.x = 190;
+  paddleCPU = new THREE.Mesh( geometry, material );
+  scene.add( paddleCPU );
+  paddleCPU.position.z = -300;
+  paddleCPU.position.x = 190;
+  paddleCPU.position.y = 0;
+  cpuPaddleDirY = (sphere.position.y - paddleCPU.position.y) * difficulty;
 }
 
 function addLight(){
@@ -152,39 +158,55 @@ function addLight(){
   scene.add(pointLight);
 }
 
+function score(){
+  if (sphere.position.x == PLANE_WIDTH/2){
+    score1 += 1;
+    document.getElementById("scores").innerHTML = (score1) + "-" + (score2 + 0);
+  }
+  if (sphere.position.x == -PLANE_WIDTH/2){
+    score2 += 1;
+    document.getElementById("scores").innerHTML = (score1 + 0) + "-" + (score2);
+  }
+  if ((score1 || score2) == maxScore){
+    alert("Has ganado! Refresca la pagina para volver a intentarlo.");
+  }
+  document.getElementById("winnerBoard").innerHTML = "First to " + maxScore + " wins!"
+}
 
-function draw()
-{
+function rebound(){
+  if (paddleCPU.position.x == sphere.position.x && ((sphere.position.y <= paddleCPU.position.y + PADDLE_HEIGHT/2) || (sphere.position.y >= paddleCPU.position.y - PADDLE_HEIGHT/2))){
+    ballDirX = -ballDirX;
+  }
+  if (paddlePlayer.position.x == sphere.position.x && ((sphere.position.y <= paddlePlayer.position.y + PADDLE_HEIGHT/2) || (sphere.position.y >= paddlePlayer.position.y - PADDLE_HEIGHT/2))){
+    ballDirX = -ballDirX;
+  }
+}
+
+function draw(){
   // Draw!
   renderer.render(scene, camera);
 
   // Schedule the next frame
   requestAnimationFrame(draw);
-  sphere.position.x += ballSpeed*ballDirX;
+
+  sphere.position.x += ballSpeed * ballDirX;
   if (sphere.position.x == PLANE_WIDTH/2 || sphere.position.x == -PLANE_WIDTH/2){
-    ballDirX = -ballDirX;
+    score();
+    sphere.position.x = ballSpeed * ballDirX;
   }
-  sphere.position.y += ballSpeed*ballDirY;
+  sphere.position.y += ballSpeed * ballDirY;
   if (sphere.position.y == PLANE_HEIGHT/2 || sphere.position.y == -PLANE_HEIGHT/2){
     ballDirY = -ballDirY;
+    score();
   }
-  sphere.rotation.x += 0.1;
-  sphere.rotation.y += 0.1;
-
-  if (Key.isDown(Key.A))
-  {
-    if (cubePlayer.position.y <= ((PLANE_HEIGHT/2) - PADDLE_HEIGHT)){
-      cubePlayer.position.y += 1;
-    }
-    if (sphere.position.x == cubePlayer.position.x &&){
-      ballDirX = -ballDirX;
+  if (Key.isDown(Key.A)){
+    if (paddlePlayer.position.y <= ((PLANE_HEIGHT/2) - PADDLE_HEIGHT/2)){
+      paddlePlayer.position.y += 1 * paddleSpeed;
     }
   }
-  if (Key.isDown(Key.D))
-  {
-    if (cubePlayer.position.y >= ((-PLANE_HEIGHT/2) + PADDLE_HEIGHT)){
-      cubePlayer.position.y -= 1;
+  if (Key.isDown(Key.D)){
+    if (paddlePlayer.position.y >= ((-PLANE_HEIGHT/2) + PADDLE_HEIGHT/2)){
+      paddlePlayer.position.y -= 1 * paddleSpeed;
     }
-
   }
 }
